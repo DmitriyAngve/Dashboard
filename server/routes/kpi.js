7cab3d42ceb274c42bd3fea3b7dd49ef1ab08378
Dmitriy Angve <kirirch129@gmail.com>
2023-04-28 16:03:00 +0300

    route finally

--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -1,13 +1,19 @@
 import express from "express";
+import KPI from "../models/KPI.js";
 
 const router = express.Router();
 
 router.get("/kpi", async (req, res) => {
   try {
-    const kpis = await KPI.find(); // grap kpis data from my database! For this I use "mongoose" - object document mapping (odm), is equvialent for noSQL DB
+    const kpis = await KPI.find();
+    // grap kpis data from my database! For this I use "mongoose" - object document mapping (odm), is equvialent for noSQL DB
+    res.status(200).json(kpis);
+    // we're sending our kpis object that we've grabbed from my database and we're it to the frontend
   } catch {
-    res.status(404).json({ message: error.message }); // if we haven't status of an error I can just sya 404
+    res.status(404).json({ message: error.message });
+    // if we haven't status of an error I can just sya 404
   }
-}); // this is the actual route what we set up,  the entry point for specific routes
+});
+// this is the actual route what we set up,  the entry point for specific routes
 
 export default router;
