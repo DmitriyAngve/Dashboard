5e6c74b7b57d9d0e3ca56d0190169499b1d44d8d
Dmitriy Angve <kirirch129@gmail.com>
2023-04-28 16:30:29 +0300

    #

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import kpiRoutes from "./routes/kpi.js"; // in node.js we need to specify the ".js"
+import kpiRoutes from "./routes/kpi.js";
+// in node.js we need to specify the ".js"
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -30,5 +33,13 @@ mongoose
   })
   .then(async () => {
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    /*
+    await mongoose.connection.db.dropDatabase();
+    // before we're going to seed our DB with information we want to drop the current DB that we already have and the reason why we do it is so what we don't have duplicate data or we don't have it we don't run into specific errors we're just going to run this particular thing once and then we're going seed that see the information as needed (in really prod need backup)
+    KPI.insertMany(kpis);
+    // we're passing in the array and by doing so we can input this information
+    */
   })
   .catch((error) => console.log(`${error} did not connect`));
