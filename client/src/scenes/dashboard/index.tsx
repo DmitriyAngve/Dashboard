23d96299a7446979e333509f8751c9b21c11e619
Dmitriy Angve <kirirch129@gmail.com>
2023-05-09 10:57:48 +0300

    Row1 done

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -59,7 +59,7 @@ const Dashboard = () => {
       sx={
         isAboveMediumScreens
           ? {
-              gridTemplateColumns: "repeat(3, minmax(370px, 1 fr))",
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
               gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
               gridTemplateAreas: gridTemplateLargeScreens,
             }
